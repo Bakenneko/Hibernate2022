1ecae1ae40b5fb9af8d667ea1cf473b21514f45c
Andrew Kurylyk <andronniy7@gmail.com>
2022-12-07 15:49:11 +0200

    dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        StandardServiceRegistry serviceRegistry =
+                new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml")
+                .build();
+
+        Metadata metadata = new MetadataSources(serviceRegistry).addAnnotatedClass(null).getMetadataBuilder().build();
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
+
     }
 }
